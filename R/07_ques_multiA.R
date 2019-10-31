3303f9a47d43a92933cc3d1d28ee940ba961e4d4
takewiki <hulilei@takewiki.com.cn>
2019-11-01 02:05:24 +0800

    update the file

--- R/07_ques_multiA.R
+++ R/07_ques_multiA.R
@@ -53,3 +53,26 @@ ques_multiA_current_lessThan <- function(brand ='JBLH',count=5){
   res <-sql_select(conn,sql)
   return(res)
 }
+
+
+
+
+#' 获取一个问题标准答案的数据
+#'
+#' @param brand 品牌
+#'
+#' @return 返回值
+#' @export
+#'
+#' @examples
+#' ques_standard_top9_list()
+ques_standard_top9_list <- function(brand='JBLH'){
+  conn <-conn_nsim();
+  sql <-sql_paste("select   FQuestionId,FQuestion,FAnswerId,FAnswer  from vw_ques_standard_current
+where FQuestionId in
+(select  top 9 FQuestionId from vw_ques_standard_current_FQuestionId_New
+where FBrand ='",brand,"') and Fbrand ='",brand,"'")
+  res <- sql_select(conn,sql);
+  res <-split(res,as.character(res$FQuestionId))
+  return(res);
+}
